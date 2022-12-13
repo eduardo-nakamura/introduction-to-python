45375a85bf0e1eaaaee4f38c347358f1ba32d1d8
eduardo-nakamura <edu-nakamura@hotmail.com>
2022-12-13 16:42:55 -0300

    Python Boolean and Conditional Programming

---
+++ 003-python-boolean-and-operators.py
@@ -0,0 +1,32 @@
+door_is_locked = False
+
+if door_is_locked:
+    print("Door is Locked")
+else:
+    print("Let's go inside")
+
+# Operators
+print('2 > 1? =', 2 > 1)
+print('2 < 1? =', 2 < 1)
+print('2 < 3 < 4 < 5 < 6? =', 2 < 3 < 4 < 5 < 6)
+print('2 < 3 > 2? =', 2 < 3 > 2)
+print('3 <= 3? =', 3 <= 3)
+print('3 >= 2? =', 3 >= 2)
+print('2 == 2? =', 2 == 2)
+print('4 != 5? =', 4 != 5)
+print('a == a? =', 'a' == 'a')
+print('a > b? =', 'a' > 'b')
+
+# logical operators
+print('not True =', not True)
+print('not False =', not False)
+print('True and True =', True and True)
+print('True and False =', True and False)
+
+# compare different types
+print('True == 1? =', True == 1)
+print('False == 0? =', False == 0)
+print('True + True =', True + True)
+print('False + False =', False + False)
+print('False + True =', False + True)
+print('True + 3 =', True + 3)
--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Table of Contents
 
 * [Python Variable](https://github.com/eduardo-nakamura/introduction-to-python/blob/master/001-variable.py)
 * [Python String: Working With Text](https://github.com/eduardo-nakamura/introduction-to-python/blob/master/002-strings.py)
+* [Python Boolean and Conditional Programming: if.. else](https://github.com/eduardo-nakamura/introduction-to-python/blob/master/003-python-boolean-and-operators.py)
